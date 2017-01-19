0173042a53f939f272d87491ccd31fe5fed894f0
Ed Rubin <edwardarubin@gmail.com>
2017-01-19 13:11:45 -0800

    add pdf; start section 2; add links in notes.Rmd

---
+++ pdfScript.R
@@ -0,0 +1,15 @@
+library(rmarkdown)
+render("section01.Rmd",
+  pdf_document(
+    latex_engine = "xelatex",
+    toc = T,
+    number_sections = T,
+    highlight = "pygments",
+    pandoc_args = c(
+      "--metadata=author:\"Ed Rubin\"",
+      pandoc_variable_arg("mainfont", "Charter"),
+      pandoc_variable_arg("monofont", "Fira Code"),
+      pandoc_variable_arg("fontsize", "11pt")
+      )
+    ),
+  output_dir = "Section01")
