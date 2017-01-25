1af6d372934c0d70a1923ceef866ab4ea7d0e851
Edward Rubin <edwardarubin@Edwards-MacBook-Pro.local>
2017-01-24 16:47:18 -0800

    section 2 notes

--- pdfScript.R
+++ pdfScript.R
@@ -1,5 +1,5 @@
 library(rmarkdown)
-render("section00.Rmd",
+render("section02.Rmd",
   pdf_document(
     latex_engine = "xelatex",
     toc = T,
@@ -12,4 +12,4 @@ render("section00.Rmd",
       pandoc_variable_arg("fontsize", "11pt")
       )
     ),
-  output_dir = "Section00")
+  output_dir = "Section02")
