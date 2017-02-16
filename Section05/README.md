a2d95df1d311c0aa2c1aa85196618385c6861a63
Ed Rubin <edwardarubin@gmail.com>
2017-02-15 16:18:32 -0800

    section 5 notes

---
+++ Section05/README.md
@@ -0,0 +1,4 @@
+# Section 5 files
+
+- PDF version of online notes.
+- CSV form of automobile data (auto.csv).
--- pdfScript.R
+++ pdfScript.R
@@ -1,5 +1,5 @@
 library(rmarkdown)
-render("section04.Rmd",
+render("section05.Rmd",
   pdf_document(
     latex_engine = "xelatex",
     toc = T,
@@ -12,4 +12,4 @@ render("section04.Rmd",
       pandoc_variable_arg("fontsize", "11pt")
       )
     ),
-  output_dir = "Section04")
+  output_dir = "Section05")
