82aaa621f2e622971fc55c8436f81cc52b79611b
Edward Rubin <edwardarubin@Edwards-MacBook-Pro.local>
2017-03-15 23:08:26 -0700

    section 9 notes

--- TODO.md
+++ TODO.md
@@ -1,3 +1,9 @@
 # TODO
 
 - Add README about building the course website.
+- Trouble-shooting code
+  - `browse()`
+- Maximum likelihood
+- Bootstrapping
+- Prediction intervals vs. confidence intervals
+- Tables with `stargazer` and/or `xtable`
--- pdfScript.R
+++ pdfScript.R
@@ -1,5 +1,5 @@
 library(rmarkdown)
-render("section08.Rmd",
+render("section09.Rmd",
   pdf_document(
     latex_engine = "xelatex",
     toc = T,
@@ -12,4 +12,4 @@ render("section08.Rmd",
       pandoc_variable_arg("fontsize", "11pt")
       )
     ),
-  output_dir = "Section08")
+  output_dir = "Section09")
