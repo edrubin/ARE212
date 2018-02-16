7ecc2a7905326614c28d41ffed3cf568a974f652
Ed Rubin <edwardarubin@gmail.com>
2018-02-16 09:40:54 -0800

    add sec 5 notes

--- pdfScript.R
+++ pdfScript.R
@@ -1,6 +1,6 @@
 library(rmarkdown)
 setwd("/Users/edwardarubin/Dropbox/Teaching/ARE212")
-render("section04.Rmd",
+render("section05.Rmd",
   pdf_document(
     latex_engine = "xelatex",
     toc = T,
@@ -13,4 +13,4 @@ render("section04.Rmd",
       pandoc_variable_arg("fontsize", "11pt")
       )
     ),
-  output_dir = "Section04")
+  output_dir = "Section05")
